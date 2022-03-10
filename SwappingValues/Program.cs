5ee16d4a8e43be31f9e5e1c78387e062e66f8b83
Sta2yan <s2_stas@mail.ru>
2022-03-10 17:41:06 +0300

    Добавьте файлы проекта.

---
+++ SwappingValues/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SwappingValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Журавлев";
+            string surname = "Станислав";
+            string tempName;
+
+            Console.WriteLine($"Имя: - {name} | Фамилия - {surname}");
+
+            tempName = name;
+            name = surname;
+            surname = tempName;
+
+            Console.WriteLine($"Имя: - {name} | Фамилия - {surname}");
+        }
+    }
+}
